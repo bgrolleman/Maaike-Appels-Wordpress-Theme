167038949c4409eab8b51be625cc345478fefee2
Bas Grolleman <bgrolleman@emendo-it.nl>
2011-04-17 23:15:09 +0200

    Moving analytics

--- header.php
+++ header.php
@@ -10,21 +10,6 @@
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<?php wp_get_archives('type=monthly&format=link'); ?>
 	<?php wp_head(); ?>
-<!-- Google Analytics -->
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-20157903-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-<!-- Google Analytics -->
 </head>
 <body>
 <a href="<?php bloginfo('url'); ?>" title="Happy face :)" class="happy-face"></a>
