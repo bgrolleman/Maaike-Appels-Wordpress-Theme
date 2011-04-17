6d7832dd4ac85c82e2df7bd1fe1a2ead13971ee2
Bas Grolleman <bgrolleman@emendo-it.nl>
2011-04-17 23:10:04 +0200

    Adding style/javascript for close hide

--- header.php
+++ header.php
@@ -10,21 +10,20 @@
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<?php wp_get_archives('type=monthly&format=link'); ?>
 	<?php wp_head(); ?>
-<!-- Google Analytics -->
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-20157903-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-<!-- Google Analytics -->
+	<script language="javascript" type="text/javascript">
+		function showHide(shID) {
+		   if (document.getElementById(shID)) {
+		      if (document.getElementById(shID+'-show').style.display != 'none') {
+			 document.getElementById(shID+'-show').style.display = 'none';
+			 document.getElementById(shID).style.display = 'block';
+		      }
+		      else {
+			 document.getElementById(shID+'-show').style.display = 'inline';
+			 document.getElementById(shID).style.display = 'none';
+		      }
+		   }
+		}
+	</script>
 </head>
 <body>
 <a href="<?php bloginfo('url'); ?>" title="Happy face :)" class="happy-face"></a>
